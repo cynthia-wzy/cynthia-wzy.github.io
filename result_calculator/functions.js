dc5cba3948be2fd858f29b5b9bb7c8bf241b5bb9
Cynthia <77578840+cynthia-wzy@users.noreply.github.com>
2023-09-04 18:04:56 +0800

    Update functions.js

--- result_calculator/functions.js
+++ result_calculator/functions.js
@@ -714,7 +714,7 @@ function drawChart(age, num1, num2, num3, num4, num5) {
 
     var data = google.visualization.arrayToDataTable([
         ["時段", "心率", { role: "annotation" }, "最大心率 (根據年齡推算)"],
-        ["運動第0分鐘", num1, "最大心率百分比" + (num1 / maxHeartRate * 100).toFixed(1) + "%", maxHeartRate],
+        ["運動第0分鐘", num1, "最大心率百分比：" + (num1 / maxHeartRate * 100).toFixed(1) + "%", maxHeartRate],
         ["運動1分鐘後", num2, (num2 / maxHeartRate * 100).toFixed(1) + "%", maxHeartRate],
         ["運動2分鐘後", num3, (num3 / maxHeartRate * 100).toFixed(1) + "%", maxHeartRate],
         ["運動3分鐘後", num4, (num4 / maxHeartRate * 100).toFixed(1) + "%", maxHeartRate],
@@ -724,6 +724,10 @@ function drawChart(age, num1, num2, num3, num4, num5) {
     // 設定圖表
     var options = {
         title: "漸進式原地抬膝踏步心率：心率變化",
+        titleTextStyle: {
+            fontSize:15,
+            bold: true
+        },
         curveType: "function", // 平滑線
         legend: { position: "bottom" }
         
